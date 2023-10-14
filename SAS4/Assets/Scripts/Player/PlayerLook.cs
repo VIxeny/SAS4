49a45196dd899c75acce10b5121fb95cf1295bf3
VIxen <Basov977@gmail.com>
2023-10-14 23:01:38 +0300

    Also Another 3 files

---
+++ SAS4/Assets/Scripts/Player/PlayerLook.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Architecture
+{
+    public class PlayerLook : MonoBehaviour
+    {
+        private const string KeyboardMouseScheme = "Keyboard&Mouse";
+        private const string GamepadScheme = "Gamepad";
+        private string _currentControlScheme;
+        private Vector2 _lookDirection;
+
+        [SerializeField] private Camera playerCamera;
+
+        public void OnControlsSchemeChanged(PlayerInput playerInput)
+        {
+            _currentControlScheme = playerInput.currentControlScheme;
+        }
+
+        private void Update()
+        {
+            switch (_currentControlScheme)
+            {
+                case KeyboardMouseScheme:
+                    
+                    break;
+                case GamepadScheme:
+                    if (_lookDirection.magnitude > 0.5f)
+                    {
+                        float angle = Mathf.Atan2(_lookDirection.y, _lookDirection.x) * Mathf.Rad2Deg - 90f;
+                        transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+                    }
+
+                    break;
+            }
+        }
+
+        public void GetGamepadLookDirection(InputAction.CallbackContext context)
+        {
+            _lookDirection = context.ReadValue<Vector2>();
+        }
+    }
+}
